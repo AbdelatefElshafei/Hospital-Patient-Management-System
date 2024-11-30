a87a8f76eeaec3cc608f2c9faf78a7a7d53c328e
Abdelrahmanhamed123 <abdelrahmanhamed1100@gmail.com>
2024-11-30 13:04:04 +0200

    Update Date.java

--- src/Date.java
+++ src/Date.java
@@ -5,8 +5,73 @@ public class Date
 
     public Date(int month, int day)
     {
+        switch(month) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                if (day > 31 || day < 1) {
+                    System.out.println("choose another day");
+
+                } else {
+                    this.month = month;
+                    this.day = day;
+                }
+                break;
+
+            case 2:
+                if (day > 28 || day < 1) {
+                    System.out.println("choose another day");
+                } else {
+                    this.month = month;
+                    this.day = day;
+                }
+                break;
+
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                if(day>30||day<1){
+                    System.out.println("choose another day");
+                }
+                else {
+                    this.month = month;
+                    this.day = day;
+                }
+                break;
+        }
+        if(month>12||month<1){
+            System.out.println("choose another month");
+        }
+    }
+
+    public int getMonth()
+    {
+        return month;
+    }
+
+    public void setMonth(int month)
+    {
         this.month = month;
+    }
+
+    public int getDay()
+    {
+        return day;
+    }
+
+    public void setDay(int day)
+    {
         this.day = day;
+    }
+
+    public void display()
+    {
+        System.out.print(day + "/" + month);
     }
 
     public static class Time
@@ -15,17 +80,43 @@ public class Date
         private int minute;
         public Time(int hour, int minute)
         {
+            if(hour>12||hour<1)
+            {
+                System.out.println("choose another hour");
+            }
+            else {
+                this.hour=hour;
+            }
+            if (minute>59||minute<0){
+                System.out.println("choose another min");
+            }
+            else {
+                this.minute=minute;
+            }
+        }
+
+        public int getHour()
+        {
+            return hour;
+        }
+
+        public void setHour(int hour)
+        {
             this.hour = hour;
+        }
+
+        public int getMinute() {
+            return minute;
+        }
+
+        public void setMinute(int minute) {
             this.minute = minute;
         }
-        @Override
-        public String toString() {
-            return String.format("%02d:%02d", hour, minute); // Format as HH:mm
+
+        public void display()
+        {
+            System.out.println(hour + ":" + minute);
         }
-    }
-    @Override
-    public String toString() {
-        return String.format("%02d-%02d", month, day); 
     }
 }
 
